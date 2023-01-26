24f3007ac0ea2c0e6742c3f44c6378c961960962
Manel <ea3eiz@ea3eiz.com>
2023-01-27 00:05:43 +0100

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
                         cd /home/pi/.local
-                        git clone http://github.com/manel1954/D1090
+                        git clone http://github.com/manel1954/A109
                         sleep 2
                         if [ -f /home/pi/.local/D1090/aviso ]
                         then
                         sudo rm -R /home/pi/A108
-                        cp -R /home/pi/.local/D1090 /home/pi
+                        cp -R /home/pi/.local/A109 /home/pi
                         cd /home/pi
-                        mv D1090 A108
+                        mv A109 A108
                         sudo chmod 777 -R A108
                         cd /home/pi/.local
-                        sudo rm -R D1090
+                        sudo rm -R A109
 
                         cd /home/pi/A108
                         ./qt_imagen_actualizada
@@ -22,7 +22,7 @@
                         echo "           ERROR DE RED            "
                         echo -n "${VERDE}"
                         echo "***********************************"
-                        sudo rm -R /home/pi/.local/D1090
+                        sudo rm -R /home/pi/.local/A109
                         echo "${GRIS}"
                         sleep 5
                         exit
